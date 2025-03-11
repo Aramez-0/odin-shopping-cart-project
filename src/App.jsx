c35ffe8634b02df52b227e3e36b35c8fe36b67c8
Aramez <191219550+Aramez-0@users.noreply.github.com>
2025-03-12 00:56:28 +1000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div id='container'>
+      <header>
+        <h1>Shopping site</h1>
+        <nav>
+          {/* home link 
+          shopping link 
+          cart link  */}
+        </nav>
+      </header>
+      <main>
+        <p>Shop to your heart's content</p>
+      </main>
+      <footer>
+        <p>Footer. Information maybe</p>
+
+      </footer>
+    </div>
+  )
+}
+
+export default App
